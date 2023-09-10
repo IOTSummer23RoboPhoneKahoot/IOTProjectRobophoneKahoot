b3f86d82f00248f414d8112aaa51b034a44465a7
Sabaa Taha <sabaataha@Sabaas-MBP.CS.Technion.ac.il>
2023-09-10 14:04:28 +0300

    addee complete quiz page, fixed bug for back to intro page button

---
+++ web_project/lib/SummaryQuizPage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'CreateQuiz.dart';
+import 'IntroPage.dart';
+import 'package:web_project/models/quiz.dart';
+import 'package:web_project/services/firebase_service.dart';
+import 'package:web_project/widgets/quiz_card.dart';
+
+class summaryPage extends StatefulWidget {
+  @override
+  _summaryPageState createState() => _summaryPageState();
+}
+
+class _summaryPageState extends State<summaryPage> {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Robophone Kahoot Game'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CreateQuizApp()),
+              );
+            },
+            child: Text('Create Quiz'),
+          ),
+        ],
+      ),
+      body:Center ( 
+        child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Text('Admin,you created your Quiz succesfully!'),
+        SizedBox(height: 40.0), 
+        ElevatedButton(
+            onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => introPage()),
+                );
+            },
+            child: Text('finish'),
+        ),
+        SizedBox(height: 10.0), // Add some space between cards
+        ],
+      ),
+      ),
+    );
+  }
+}
