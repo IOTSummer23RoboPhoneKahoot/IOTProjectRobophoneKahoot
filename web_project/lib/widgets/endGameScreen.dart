083e556833d6e15dea0ab4eb17bb0802d960609b
RuqayaKhalil <131773723+RuqayaKhalil@users.noreply.github.com>
2023-09-14 19:32:20 +0300

    Create endGameScreen.dart

---
+++ web_project/lib/widgets/endGameScreen.dart
@@ -0,0 +1,40 @@
+import 'package:web_project/widgets/TopScoreWidget.dart';
+import 'package:web_project/widgets/fastestPlayerWidget.dart';
+import 'package:web_project/widgets/correctEachQuestionWidget.dart';
+import 'package:web_project/widgets/topNWinners.dart';
+import 'package:flutter/material.dart';
+import 'package:web_project/models/quiz.dart';
+
+class EndGameScreen extends StatefulWidget {
+  final Quiz quiz;
+
+  EndGameScreen({required this.quiz});
+
+  @override
+  _EndGameScreen createState() => _EndGameScreen();
+}
+
+class _EndGameScreen extends State<EndGameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('End Game Screen'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            HighestScoreWidget(quiz: widget.quiz),
+            SizedBox(height: 10),
+            FastestPlayerWidget(quiz: widget.quiz),
+            SizedBox(height: 10),
+            TopNWinners(quiz: widget.quiz),
+            SizedBox(height: 10),
+            CorrectAnswersWidget(quiz: widget.quiz),
+          ],
+        ),
+      ),
+    );
+  }
+}
